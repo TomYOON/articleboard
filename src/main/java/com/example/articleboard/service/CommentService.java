c0bf9a5ca8a6cd0765637ad209500dddd4aa34d5
TomYOON <31751481+TomYOON@users.noreply.github.com>
2022-05-07 23:24:57 +0900

    Create CommentService.java

---
+++ src/main/java/com/example/articleboard/service/CommentService.java
@@ -0,0 +1,45 @@
+package com.example.articleboard.service;
+
+import com.example.articleboard.domain.Article;
+import com.example.articleboard.domain.Comment;
+import com.example.articleboard.domain.Member;
+import com.example.articleboard.repository.ArticleRepository;
+import com.example.articleboard.repository.CommentRepository;
+import com.example.articleboard.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final MemberRepository memberRepository;
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+
+    public Long write(Long memberId, Long articleId, Long parentId, Long tagMemberId, String content) {
+        Member member = memberRepository.findOne(memberId);
+        Article article = articleRepository.findOne(articleId);
+        Comment parentComment = null;
+        Member tagMember = null;
+
+        if (parentId != null) {
+            parentComment = commentRepository.findOne(parentId);
+        }
+
+        if (tagMemberId != null) {
+            tagMember = memberRepository.findOne(tagMemberId);
+        }
+
+        Comment comment = Comment.createComment(member, article, parentComment, tagMember, content);
+        commentRepository.save(comment);
+
+        return comment.getId();
+    }
+
+    public List<Comment> findCommentByMemberId(Long memberId, int offset, int limit) {
+        return commentRepository.findByMemberId(memberId, offset, limit);
+    }
+}
