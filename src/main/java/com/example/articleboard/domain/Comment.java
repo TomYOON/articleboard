ec769433be65efca9ddb5fa321341bbee664eafb
TomYOON <31751481+TomYOON@users.noreply.github.com>
2022-05-07 23:28:40 +0900

    feat: add create method and relation method

--- src/main/java/com/example/articleboard/domain/Comment.java
+++ src/main/java/com/example/articleboard/domain/Comment.java
@@ -1,6 +1,8 @@
 package com.example.articleboard.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     @Id
@@ -17,7 +20,7 @@ public class Comment {
 
     @ManyToOne
     @JoinColumn(name = "member_id")
-    private Member writer;
+    private Member member;
 
     @ManyToOne
     @JoinColumn(name = "article_id")
@@ -35,5 +38,48 @@ public class Comment {
 
     @OneToMany(mappedBy = "parentComment")
     private List<Comment> childComments = new ArrayList<>();
+
+    //== 연관관계 메서드 ==//
+    private void setMember(Member member) {
+        this.member = member;
+    }
+
+    private void setArticle(Article article) {
+        this.article = article;
+        article.getComments().add(this);
+    }
+
+    private void setParentComment(Comment parentComment) {
+        this.parentComment = parentComment;
+        parentComment.getChildComments().add(this);
+    }
+
+    private void setTagMember(Member tagMember) {
+        this.tagMember = tagMember;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    //== 생성 메서드 ==//
+    public static Comment createComment(Member member, Article article, Comment parentComment, Member tagMember, String content) {
+        Comment comment = new Comment();
+        comment.setMember(member);
+        comment.setArticle(article);
+        comment.setContent(content);
+
+        if (parentComment != null) {
+            comment.setParentComment(parentComment);
+        } else {
+            comment.setParentComment(comment);
+        }
+
+        if (tagMember != null) {
+            comment.setTagMember(tagMember);
+        }
+
+        return comment;
+    }
 
 }
